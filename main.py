7a3aa4265946ea2599c737cc4dfb259ba530d570
zara01991 <74278422+zara01991@users.noreply.github.com>
2023-04-12 22:56:26 -0400

    added rounds

--- main.py
+++ main.py
@@ -10,50 +10,85 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-snake = Snake()
-food = Food()
-race_is_on = True
-score = Score()
-
-# game starts
-while race_is_on:
-    screen.update()
-    time.sleep(0.15)
-    #hit_tail = False
-    #i = 2
-
-    P = []
-    for i in snake.segment[2:]:
-        position = (round(i.xcor()),round(i.ycor()))
-        P.append(position)
-    head_position = (round(snake.head.xcor()), round(snake.head.ycor()))
-    #print(P)
-    #print(head_position)
-    if snake.head.xcor() >= 295 or snake.head.xcor() <= -295 or snake.head.ycor() >= 295 or snake.head.ycor() <= -295 or head_position in P:
-        race_is_on = False
-        score.home()
-        score.game_over()
-
-
-        # while hit_tail == False and i < len(snake.segment):
-        #     if snake.head.xcor() == snake.segment[i].xcor() and snake.head.ycor() == snake.segment[i].ycor():
-        #         hit_tail = True
-        #         race_is_on = False
-        #
-        #     else:
-        #         i += 1
-    else:
-        snake.move()
-        if snake.head.distance(food) < 15:
-            score.add_score()
-            food.clear()
-            snake.add_tail()
-            food = Food()
 
 
 
+def game (high_score_no):
+    
+    
+    screen.clear()    
+    screen.bgcolor("black")
+
+    screen.tracer(0)
+   
+    snake = Snake()
+    food = Food()
+    race_is_on = True
+    
+    #scoreboard = Score()
+    
+    score_no = 0
+   
+    score = Score(score_no,high_score_no)
+
+    score.goto(50, 280)
+    score.write(score_no,False, align ="center", font = ('Arial', 15, 'normal') )
+    #high_score.goto(170, 280)
+    score.goto(170, 280)
+    score.write(high_score_no,False, align ="center", font = ('Arial', 15, 'normal') )
+
+    # game starts
 
 
 
+    while race_is_on:
+        screen.update()
+        time.sleep(0.15)
+        #hit_tail = False
+        #i = 2
+
+        P = []
+        for i in snake.segment[2:]:
+            position = (round(i.xcor()),round(i.ycor()))
+            P.append(position)
+        head_position = (round(snake.head.xcor()), round(snake.head.ycor()))
+        #print(P)
+        #print(head_position)
+        if snake.head.xcor() >= 295 or snake.head.xcor() <= -295 or snake.head.ycor() >= 295 or snake.head.ycor() <= -295 or head_position in P:
+            race_is_on = False
+            score.home()
+            #score.game_over()
+            score.clear()
+            if score_no > high_score_no:
+                high_score_no = score_no
+                
+            score = Score(score_no,high_score_no)
+            score.highest_score()
+            game(high_score_no)
+
+
+            # while hit_tail == False and i < len(snake.segment):
+            #     if snake.head.xcor() == snake.segment[i].xcor() and snake.head.ycor() == snake.segment[i].ycor():
+            #         hit_tail = True
+            #         race_is_on = False
+            #
+            #     else:
+            #         i += 1
+        else:
+            snake.move()
+            if snake.head.distance(food) < 15:
+                score.clear()
+                score_no += 1
+                score = Score(score_no,high_score_no)
+                score.add_score()
+                food.clear()
+                snake.add_tail()
+                food = Food()
+
+
+
+
+
+game(0)
 
 screen.exitonclick ()
--- score.py
+++ score.py
@@ -1,21 +1,41 @@
 from turtle import Turtle
 
 class Score(Turtle):
-    def __init__(self):
+    def __init__(self,score_no, high_score_no):
         super().__init__()
         self.color("white")
         self.penup()
         self.hideturtle()
         self.goto(0,280)
         self.write("score is: ",False, align="center", font = ('Arial', 15, 'normal'))
-        self.s = 0
-        self.goto(50, 280)
-        self.write(self.s,False, align ="center", font = ('Arial', 15, 'normal') )
+        self.goto(120,280)
+        self.write("highest score: ",False, align="center", font = ('Arial', 15, 'normal'))
+        self.s = score_no
+        self.h = high_score_no
+        # self.goto(50, 280)
+        # self.write(self.s,False, align ="center", font = ('Arial', 15, 'normal') )
+        # self.goto(170, 280)
+        # self.write(self.h,False, align ="center", font = ('Arial', 15, 'normal') )
 
     def add_score(self):
-        self.undo()
-        self.s += 1
-        self.write(self.s, False, align="center", font=('Arial', 15, 'normal'))
 
-    def game_over(self):
-        self.write("GAME OVER", False, align="center", font=('Arial', 25, 'bold'))
+        # self.clear()
+        #self.s += 1
+        self.goto(50, 280)
+        self.write(self.s, False, align="center", font=('Arial', 15, 'normal'))
+        self.goto(170, 280)
+        self.write(self.h, False, align="center", font=('Arial', 15, 'normal'))
+        # self.goto(170, 280)
+        # self.write(self.h, False, align="center", font=('Arial', 15, 'normal'))
+
+    # def game_over(self):
+    #     self.write("GAME OVER", False, align="center", font=('Arial', 25, 'bold'))
+
+
+    def highest_score(self):
+        self.goto(150, 280)
+        # if self.s > self.h:
+        #     self.write(self.s, False, align="center", font=('Arial', 15, 'normal'))
+
+        # else:
+        self.write(self.h, False, align="center", font=('Arial', 15, 'normal'))
